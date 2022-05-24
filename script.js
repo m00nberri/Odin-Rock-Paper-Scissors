5dd5785116af25699f31cec22dd3795615e40e5a
m00nberri <zaberkat@gmail.com>
2022-05-24 14:45:22 +0900

    Add character select following main menu

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function playGame () {
 let container = document.querySelector('#container')
 
 let mainMenu = document.createElement('div');
-mainMenu.setAttribute('class', 'menu');
+mainMenu.setAttribute('class', 'columnMenu');
 
 let mainText = document.createElement('p');
 mainText.textContent = 'Super Smash Janken';
@@ -72,8 +72,58 @@ mainMenu.appendChild(mainButton);
 container.appendChild(mainMenu);
 
 
+let characterSelectMenu = document.createElement('div');
+characterSelectMenu.setAttribute('class', 'columnMenu');
 
+let characterSelectText = document.createElement('p')
+characterSelectText.textContent = 'Choose Your Fighter!'
+
+let shuri = document.createElement('button');
+shuri.setAttribute('class', 'characterSelector')
+shuri.innerHTML = '<img src="img/shuri.png" />'
+let kani = document.createElement('button');
+kani.setAttribute('class', 'characterSelector')
+kani.innerHTML = '<img src="img/kani.png" />'
+let sinu = document.createElement('button');
+sinu.setAttribute('class', 'characterSelector')
+sinu.innerHTML = '<img src="img/sinu.png" />'
+let ninu = document.createElement('button');
+ninu.setAttribute('class', 'characterSelector')
+ninu.innerHTML = '<img src="img/ninu.png" />'
+
+let characterSelect = document.createElement('div')
+characterSelect.setAttribute('class', 'rowMenu')
+
+characterSelect.appendChild(shuri);
+characterSelect.appendChild(kani);
+characterSelect.appendChild(sinu);
+characterSelect.appendChild(ninu);
+
+characterSelectMenu.appendChild(characterSelectText);
+characterSelectMenu.appendChild(characterSelect);
+let characterSelection = ''
 
 mainButton.addEventListener('click', () => {
     container.removeChild(mainMenu);
+    container.appendChild(characterSelectMenu);
   });
+
+shuri.addEventListener('click', () => {
+    characterSelection = 'Shuri';
+    container.removeChild(characterSelectMenu);
+})
+
+kani.addEventListener('click', () => {
+    characterSelection = 'Kani';
+    container.removeChild(characterSelectMenu);
+})
+
+sinu.addEventListener('click', () => {
+    characterSelection = 'Sinu';
+    container.removeChild(characterSelectMenu);
+})
+
+ninu.addEventListener('click', () => {
+    characterSelection = 'Ninu';
+    container.removeChild(characterSelectMenu);
+})
